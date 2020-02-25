287cc1463e312dabef7477bf799da9a0c65ef2a6
BranceLee <475815059@qq.com>
2020-02-25 23:17:35 +0800

    add hashrouter module

--- webpack4.4v/src/index.js
+++ webpack4.4v/src/index.js
@@ -1,12 +1,22 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, Link, Switch } from 'react-router-dom';
 import './index.scss';
 import Modal from './components/Modal.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
 
 function App() {
   return (
     <div>
       <Modal />
+      <HashRouter>
+        <Link to="/about">about</Link>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
