69250e7d9df307005ae68df7fb393e3f998c09a8
BranceLee <475815059@qq.com>
2020-07-21 08:50:24 +0800

    rm: disable the local Cert from webpack dev

--- webpack4.4v/config/webpack.dev.js
+++ webpack4.4v/config/webpack.dev.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+// const fs = require('fs');
 const merge = require('webpack-merge');
 
 const common = require('../webpack.config.js');
@@ -10,11 +10,11 @@ module.exports = merge(common, {
     contentBase: '../dist',
     host: '0.0.0.0',
     port: 3000,
-    hot: true,
-    https: {
-      key: fs.readFileSync('/Users/lee/server.key'),
-      cert: fs.readFileSync('/Users/lee/server.crt'),
-      ca: fs.readFileSync('/Users/lee/rootCA.pem')
-    }
+    hot: true
+    // https: {
+    //   key: fs.readFileSync('/Users/lee/server.key'),
+    //   cert: fs.readFileSync('/Users/lee/server.crt'),
+    //   ca: fs.readFileSync('/Users/lee/rootCA.pem')
+    // }
   }
 });
